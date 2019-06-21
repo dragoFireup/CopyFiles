c87fe9df3fada8564cccc58d2a140c446b987ca3
R Vrishab Jain <jainvrishab@gmail.com>
2019-06-21 15:45:32 +0530

    Source File updated

--- CopyFiles.py
+++ CopyFiles.py
@@ -1,36 +1,59 @@
-import string
+'''
+	Automatically copies any files or folders inside a Folder named Test from a new USB
+
+'''
 from ctypes import windll
-import time
 import os
+import time
+import string
 from glob import glob
 
-'''
-Function to get all the logical drives in use
-'''
-def get_drives():
-    drives = []
-    bitmask = windll.kernel32.GetLogicalDrives()
-    for letter in string.ascii_uppercase:
-        if bitmask & 1:
-            drives.append(letter)
-        bitmask >>= 1
-    return drives
+def copyDir(dirname, path):
+	global copydirname
+	files = glob(dirname+'\\*')
+	for file in files:
+		if os.path.isdir(file):
+			dirname = file.split('\\')[-1]
+			newdir = path+'\\'+dirname
+			os.mkdir(newdir)
+			copyDir(file, newdir)
+		else:
+			os.system('copy '+file+' '+path)
 
+def getdrives():
 
-if __name__ == '__main__':
-    while 1:
-        before = set(get_drives())
-        time.sleep(5)
-        after = set(get_drives())
-        drives = after - before
-        delta = len(drives)
+	drives = []
+	bitmask = windll.kernel32.GetLogicalDrives()
+	for i in string.ascii_uppercase:
+		if bitmask&1:
+			drives.append(i)
+		bitmask>>=1
+	return drives
 
-        if (delta):
-            for drive in drives:
-                files = glob(drive+":\\*.docx")
-                for _ in files:
-                    print(_)
-                    os.system("copy " + _ + " D:\\Download")
-                    time.sleep(1)
-                if len(files) == 0:
-                    print("No PPT in the "+drive+" drive")
+if __name__ == "__main__":
+	global copydirname
+	print('Please make sure that all the contents that you want to copy are in the test folder in the USB drive')
+	while 1:
+		before = set(getdrives())
+		time.sleep(1)
+		after = set(getdrives())
+		drives = after - before
+
+		delta = len(drives)
+
+		if delta:
+			for drive in drives:
+				files = glob(drive+":\\test\\*")
+				if len(files) == 0:
+					print('No folder named Test in '+drive+' drive')
+					continue
+				copydirname = input('Enter the absolute path of the destination for '+drive+':\\test contents: ')
+				for file in files:
+					if os.path.isdir(file):
+						dirname = file.split('\\')[-1]
+						newdir = copydirname+'\\'+dirname
+						os.mkdir(newdir)
+						copyDir(file, newdir)
+					else:
+						os.system('copy '+file+' '+copydirname)
+				
